4539c8a91254e542e77cdda30f3677ba95028fc1
Jean Carlos <jeancarlmg@gmail.com>
2025-03-14 18:19:04 -0500

    Model and Controller

---
+++ MagicVilla_API/Controllers/VillaController.cs
@@ -0,0 +1,20 @@
+﻿using MagicVilla_API.Modelos;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace MagicVilla_API.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class VillaController : ControllerBase
+    {
+        public  IEnumerable<Villa> GetVillas()
+        {
+            return new List<Villa>
+            {
+                new Villa { Id = 1, Nombre = "Vista a la Piscina" },
+                new Villa { Id = 1, Nombre = "Vista a la Playa" }
+            };
+        }
+    }
+}
--- MagicVilla_API/obj/Debug/net9.0/MagicVilla_API.AssemblyInfo.cs
+++ MagicVilla_API/obj/Debug/net9.0/MagicVilla_API.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("MagicVilla_API")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+e5660619dd77d8a98fbffea36ac54ae97a60dfa0")]
 [assembly: System.Reflection.AssemblyProductAttribute("MagicVilla_API")]
 [assembly: System.Reflection.AssemblyTitleAttribute("MagicVilla_API")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
